d6e9a1e2bb86a654797076b70bf4f6263dab5e90
João Lucas <77590858+dev-stupid-codes@users.noreply.github.com>
2025-08-06 11:56:37 -0400

    feat: implement dynamic payment method buttons with flavor-specific support

---
+++ app/src/stone/java/br/com/ticpass/pos/payment/models/SupportedPaymentMethods.kt
@@ -0,0 +1,16 @@
+package br.com.ticpass.pos.payment.models
+
+/**
+ * Stone supported payment methods
+ */
+object SupportedPaymentMethods {
+    val methods = listOf(
+//        SystemPaymentMethod.CREDIT,
+//        SystemPaymentMethod.DEBIT,
+//        SystemPaymentMethod.VOUCHER,
+        SystemPaymentMethod.PIX,
+        SystemPaymentMethod.CASH,
+        SystemPaymentMethod.LN_BITCOIN,
+        SystemPaymentMethod.MERCHANT_PIX,
+    )
+}
