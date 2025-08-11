743d54b32964acc046a37c619ca2f49a4571b9f2
Gabriel Ramos <spardevv@gmail.com>
2025-08-11 11:26:03 -0400

    feat: finish support screen

--- app/src/main/java/br/com/ticpass/pos/data/activity/SupportActivity.kt
+++ app/src/main/java/br/com/ticpass/pos/data/activity/SupportActivity.kt
@@ -1,4 +1,19 @@
 package br.com.ticpass.pos.data.activity
 
-class SupportActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.ticpass.pos.databinding.ActivitySupportBinding
+
+class SupportActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySupportBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySupportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Fale Conosco"
+    }
 }
